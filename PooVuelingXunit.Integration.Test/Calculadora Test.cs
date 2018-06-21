fbe2a5d09917e03c1c1a8c4e4581f95a229e797a
rogercalata <35268126+rogercalata@users.noreply.github.com>
2018-06-21 22:41:01 +0200

    primer commit
    subida de repo

---
+++ PooVuelingXunit.Integration.Test/Calculadora Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PooVuelingXunit.Integration.Test
+{
+    public class Calculadora_Test
+    {
+        ICalculadora iCalculadora = new Calculadora();
+        private readonly Calculadora _Calculadora;
+
+        public Calculadora_Test()
+        {
+            _Calculadora = new Calculadora();
+        }
+
+        [Fact]
+        public void Suma()
+        {
+            var result = _Calculadora.Suma(1, 2);
+
+            Assert.True(iCalculadora.Suma(num1, num2)==result);
+        }
+
+    }
+}
