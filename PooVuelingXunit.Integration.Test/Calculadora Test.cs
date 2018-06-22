19dbafc7d72a92f205414a48e4e9e0f828d4df24
rogercalata <35268126+rogercalata@users.noreply.github.com>
2018-06-22 10:59:43 +0200

    integration.test
    dessarrollo de los test dee integracion, y creacion del unit.test

--- PooVuelingXunit.Integration.Test/Calculadora Test.cs
+++ PooVuelingXunit.Integration.Test/Calculadora Test.cs
@@ -17,13 +17,38 @@ namespace PooVuelingXunit.Integration.Test
             _Calculadora = new Calculadora();
         }
 
-        [Fact]
-        public void Suma()
+        [Theory]
+        [InlineData(5, 10, 15)]
+        [InlineData(2, 1, 3)]
+        public void Suma(int num1, int num2, int result)
         {
-            var result = _Calculadora.Suma(1, 2);
-
-            Assert.True(iCalculadora.Suma(num1, num2)==result);
+            Assert.Equal(result, num1 + num2);
         }
+
+        [Theory]
+        [InlineData(10, 5, 15)]
+        [InlineData(2, 1, 1)]
+        public void Resta(int num1, int num2, int result)
+        {
+            Assert.Equal(result, num1 - num2);
+        }
+
+        [Theory]
+        [InlineData(10, 5, 2)]
+        [InlineData(2, 1, 1)]
+        public void Division(int num1, int num2, int result)
+        {
+            Assert.Equal(result, num1 % num2);
+        }
+
+        [Theory]
+        [InlineData(10, 5, 50)]
+        [InlineData(2, 1, 2)]
+        public void Multiplicacion(int num1, int num2, int result)
+        {
+            Assert.Equal(result, num1 * num2);
+        }
+       
 
     }
 }
---
+++ PooVuelingXunit.Unit.Tests/CalculadoraTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVuelingXunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVuelingXunit.Tests
+{
+    [TestClass()]
+    public class CalculadoraTests
+    {
+        [TestMethod()]
+        public void s()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplicacionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
